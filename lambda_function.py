553bf5299f4f806568f3654245851ad0a40a38dd
dom118247 <118201442+dom118247@users.noreply.github.com>
2025-04-07 18:13:17 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
 def lambda_handler(event, context):
+    name = event.get("name", "Guest")
     return {
         'statusCode': 200,
-        'body': 'Doms AWS PROJECT USING LAMBDA & Python'
+        'body': f'Hello, {name}! Welcome to Dom\'s AWS project.'
     }
