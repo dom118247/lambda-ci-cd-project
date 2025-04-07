f6064db6c84706473caf303196f5e445da7aa7fc
dom118247 <118201442+dom118247@users.noreply.github.com>
2025-04-07 18:11:51 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Doms AWS PROJECT USING LAMBDA'
+        'body': 'Doms AWS PROJECT USING LAMBDA & Python'
     }
